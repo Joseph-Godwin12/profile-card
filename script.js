790729782ac43066fcf74e5c27b4f7a3799a635e
Egbucha Joseph <josephgodwin768@gmail.com>
2025-01-30 11:40:27 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     const currentTimeUTC = document.getElementById("current-time");
-    function updateTime() {
     const date = new Date();
-    currentTimeUTC.textContent = `Current Time: ${date.toUTCString()}`;
-    }
-
-    updateTime();
-
-    setInterval(updateTime, 1000);
+    currentTimeUTC.textContent = `Current Time: ${date.toUTCString()}`;  
 });
